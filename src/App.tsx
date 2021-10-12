b37f84bf7a6df0a9d6e197cbd991989b9d244680
Leonardo Hornig <leonardohornig321@gmail.com>
2021-10-12 20:35:47 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import {Button} from "./components/Button";
+function App() {
+  return (
+    
+    <div>
+
+    <Button text = "leo"/>
+    <Button />
+    </div>
+
+  );
+}
+
+export default App;
