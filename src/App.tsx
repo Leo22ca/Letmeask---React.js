0b5777d3e3e0c25e244a0146405511d16512892b
Leonardo Hornig <leonardohornig321@gmail.com>
2021-10-30 18:12:56 -0300

    Finalização da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import {Button} from "./components/Button";
+import { Home } from "./pages/Home";
+
 function App() {
   return (
-    
-    <div>
-
-    <Button text = "leo"/>
-    <Button />
-    </div>
-
+    <Home></Home>
   );
 }
 
